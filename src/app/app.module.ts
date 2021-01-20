f9525bd1f1b3868a5f2939429cecdf06390bb595
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2021-01-19 23:42:03 -0300

    criados componentes do layout base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { NabvarComponent } from './components/core/nabvar/nabvar.component';
+import { SidebarComponent } from './components/core/sidebar/sidebar.component';
+import { ContentComponent } from './components/core/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NabvarComponent,
+    SidebarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
